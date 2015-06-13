b65992c8724c9d2f271fa2d056f376b131f4243f
jlopez1224 <jlopez1224@gmail.com>
2015-06-13 14:09:54 -0400

    Add git_push script to auto-increment version.

---
+++ git_push.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+mkdir ../temp
+cd ../temp
+
+git config --global user.email "jlopez@twocentz.co"
+git config --global user.name "Codeship"
+git clone git@github.com:twocentz/twocentz-ui.git
+cd twocentz-ui
+
+# Update version
+line=`cat package.json | jq '.version' | sed 's/"//g'` 
+echo $line
+IFS='\\.' read -a arr <<< "$line"
+
+version="${arr[0]}.${arr[1]}.$(( ${arr[2]} + 1))" 
+echo "New version: $version"
+str="s/version\": \"$line/version\": \"$version/"
+sed -i "$str" package.json
+
+# Push version to github
+git add .
+git commit -m "[skip ci] Updating version to `cat version`"
+git push
