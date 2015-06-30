32de2ff55ae4b268e9fa43bfa755e0498cc451d1
gchaturvedi <gchaturvedi303@gmail.com>
2015-06-30 16:12:55 -0400

    Expose topic endpoints in node api

--- server/routes.js
+++ server/routes.js
@@ -19,6 +19,8 @@ var async = require('async');
 var request = require('request');
 var xml2js = require('xml2js');
 
+var API_VERSION = "1.0";
+var API_URL = "https://twocentz-svc-stage.herokuapp.com/" + API_VERSION + "/";
 
 var _ = require('lodash');
 var spMiddleware = stormpathExpressSdk.createMiddleware();
@@ -92,108 +94,29 @@ module.exports = function(app) {
 
   mongoose.connect('mongodb://admin:password@ds049161.mongolab.com:49161/campaign-yo');
 
-  app.get('/api/shows',  function(req, res, next) {
-    var query = Show.find();
-    if (req.query.genre) {
-      query.where({ genre: req.query.genre });
-    } else if (req.query.alphabet) {
-      query.where({ name: new RegExp('^' + '[' + req.query.alphabet + ']', 'i') });
-    } else {
-      query.limit(12);
-    }
-    query.exec(function(err, shows) {
-      if (err) return next(err);
-      res.send(shows);
+  app.get('/api/topics',  function(req, res, next) {
+    request({
+      url: API_URL + "topics",
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      },
+    }, function(error, response, body) {
+      console.log(body);
+      res.status(200).json(JSON.parse(body));
     });
   });
 
-  //app.get('/api/shows/:id', spMiddleware.authenticate, function(req, res, next) {
-  app.get('/api/shows/:id',  function(req, res, next) {
-    Show.findById(req.params.id, function(err, show) {
-      if (err) return next(err);
-      res.send(show);
-    });
-  });
-
-  app.post('/api/shows', function (req, res, next) {
-    var seriesName = req.body.showName
-      .toLowerCase()
-      .replace(/ /g, '_')
-      .replace(/[^\w-]+/g, '');
-    var apiKey = '9EF1D1E7D28FDA0B';
-    var parser = xml2js.Parser({
-      explicitArray: false,
-      normalizeTags: true
-    });
-
-    async.waterfall([
-      function (callback) {
-        request.get('http://thetvdb.com/api/GetSeries.php?seriesname=' + seriesName, function (error, response, body) {
-          if (error) return next(error);
-          parser.parseString(body, function (err, result) {
-            if (!result.data.series) {
-              return res.send(400, { message: req.body.showName + ' was not found.' });
-            }
-            var seriesId = result.data.series.seriesid || result.data.series[0].seriesid;
-            callback(err, seriesId);
-          });
-        });
+  app.get('/api/topics/:topic_id',  function(req, res, next) {
+    request({
+      url: API_URL + "topics" + "/" + req.params.topic_id,
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
       },
-      function (seriesId, callback) {
-        request.get('http://thetvdb.com/api/' + apiKey + '/series/' + seriesId + '/all/en.xml', function (error, response, body) {
-          if (error) return next(error);
-          parser.parseString(body, function (err, result) {
-            var series = result.data.series;
-            var episodes = result.data.episode;
-            var show = new Show({
-              _id: series.id,
-              name: series.seriesname,
-              airsDayOfWeek: series.airs_dayofweek,
-              airsTime: series.airs_time,
-              firstAired: series.firstaired,
-              genre: series.genre.split('|').filter(Boolean),
-              network: series.network,
-              overview: series.overview,
-              rating: series.rating,
-              ratingCount: series.ratingcount,
-              runtime: series.runtime,
-              status: series.status,
-              poster: series.poster,
-              episodes: []
-            });
-            _.each(episodes, function (episode) {
-              show.episodes.push({
-                season: episode.seasonnumber,
-                episodeNumber: episode.episodenumber,
-                episodeName: episode.episodename,
-                firstAired: episode.firstaired,
-                overview: episode.overview
-              });
-            });
-            callback(err, show);
-          });
-        });
-      },
-      function (show, callback) {
-        var url = 'http://thetvdb.com/banners/' + show.poster;
-        request({ url: url, encoding: null }, function (error, response, body) {
-          show.poster = 'data:' + response.headers['content-type'] + ';base64,' + body.toString('base64');
-          callback(error, show);
-        });
-      }
-    ], function (err, show) {
-      if (err) return next(err);
-      show.save(function (err) {
-        if (err) {
-          if (err.code == 11000) {
-            return res.send(409, { message: show.name + ' already exists.' });
-          }
-          return next(err);
-        }
-        var alertDate = Date.create('Next ' + show.airsDayOfWeek + ' at ' + show.airsTime).rewind({ hour: 2});
-        //agenda.schedule(alertDate, 'send email alert', show.name).repeatEvery('1 week');
-        res.send(200);
-      });
+    }, function(error, response, body) {
+      console.log(body);
+      res.status(200).json(JSON.parse(body));
     });
   });
 
