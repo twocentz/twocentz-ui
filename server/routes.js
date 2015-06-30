76500b9a035d954b8cc3ea78db5ea6801f8379c7
Ilyas Khawaja <kmilyas@gmail.com>
2015-06-29 22:59:48 -0400

    fixed stormpath login issue. now working with cookie-parser module

--- server/routes.js
+++ server/routes.js
@@ -27,7 +27,7 @@ module.exports = function(app) {
 
    //attaching stormpath middleware
   spMiddleware.attachDefaults(app);
-  
+
   var showSchema = new mongoose.Schema({
     _id: Number,
     name: String,
@@ -91,130 +91,6 @@ module.exports = function(app) {
   var Show = mongoose.model('Show', showSchema);
 
   mongoose.connect('mongodb://admin:password@ds049161.mongolab.com:49161/campaign-yo');
-
-  function ensureAuthenticated(req, res, next) {
-    if (req.headers.authorization) {
-      var token = req.headers.authorization.split(' ')[1];
-      try {
-        var decoded = jwt.decode(token, tokenSecret);
-        if (decoded.exp <= Date.now()) {
-          res.send(400, 'Access token has expired');
-        } else {
-          req.user = decoded.user;
-          return next();
-        }
-      } catch (err) {
-        return res.send(500, 'Error parsing token');
-      }
-    } else {
-      return res.send(401);
-    }
-  }
-
-  function createJwtToken(user) {
-    var payload = {
-      user: user,
-      iat: new Date().getTime(),
-      exp: moment().add('days', 7).valueOf()
-    };
-    return jwt.encode(payload, tokenSecret);
-  }
-
- 
-
-  // Insert routes below
-  app.post('/auth/signup', function(req, res, next) {
-    var user = new User({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password
-    });
-    user.save(function(err) {
-      if (err) return next(err);
-      res.send(200);
-    });
-  });
-
-  app.post('/auth/login', function(req, res, next) {
-    User.findOne({ email: req.body.email }, function(err, user) {
-      if (!user) return res.send(401, 'User does not exist');
-      user.comparePassword(req.body.password, function(err, isMatch) {
-        if (!isMatch) return res.send(401, 'Invalid email and/or password');
-        var token = createJwtToken(user);
-        res.send({ token: token });
-      });
-    });
-  });
-
-  app.post('/auth/facebook', function(req, res, next) {
-    var profile = req.body.profile;
-    var signedRequest = req.body.signedRequest;
-    var encodedSignature = signedRequest.split('.')[0];
-    var payload = signedRequest.split('.')[1];
-
-    var appSecret = '298fb6c080fda239b809ae418bf49700';
-
-    var expectedSignature = crypto.createHmac('sha256', appSecret).update(payload).digest('base64');
-    expectedSignature = expectedSignature.replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '');
-
-    if (encodedSignature !== expectedSignature) {
-      return res.send(400, 'Invalid Request Signature');
-    }
-
-    User.findOne({ facebook: profile.id }, function(err, existingUser) {
-      if (existingUser) {
-        var token = createJwtToken(existingUser);
-        return res.send(token);
-      }
-      var user = new User({
-        name: profile.name,
-        facebook: {
-          id: profile.id,
-          email: profile.email
-        }
-      });
-      user.save(function(err) {
-        if (err) return next(err);
-        var token = createJwtToken(user);
-        res.send(token);
-      });
-    });
-  });
-
-  app.post('/auth/google', function(req, res, next) {
-    var profile = req.body.profile;
-    User.findOne({ google: profile.id }, function(err, existingUser) {
-      if (existingUser) {
-        var token = createJwtToken(existingUser);
-        return res.send(token);
-      }
-      var user = new User({
-        name: profile.displayName,
-        google: {
-          id: profile.id,
-          email: profile.emails[0].value
-        }
-      });
-      user.save(function(err) {
-        if (err) return next(err);
-        var token = createJwtToken(user);
-        res.send(token);
-      });
-    });
-  });
-
-  // app.get('/api/users', function(req, res, next) {
-  //   if (!req.query.email) {
-  //     return res.send(400, { message: 'Email parameter is required.' });
-  //   }
-
-  //   User.findOne({ email: req.query.email }, function(err, user) {
-  //     if (err) return next(err);
-  //     res.send({ available: !user });
-  //   });
-  // });
-
-
 
   app.get('/api/shows',  function(req, res, next) {
     var query = Show.find();
@@ -316,29 +192,6 @@ module.exports = function(app) {
         }
         var alertDate = Date.create('Next ' + show.airsDayOfWeek + ' at ' + show.airsTime).rewind({ hour: 2});
         //agenda.schedule(alertDate, 'send email alert', show.name).repeatEvery('1 week');
-        res.send(200);
-      });
-    });
-  });
-
-  app.post('/api/subscribe', ensureAuthenticated, function(req, res, next) {
-    Show.findById(req.body.showId, function(err, show) {
-      if (err) return next(err);
-      show.subscribers.push(req.user._id);
-      show.save(function(err) {
-        if (err) return next(err);
-        res.send(200);
-      });
-    });
-  });
-
-  app.post('/api/unsubscribe', ensureAuthenticated, function(req, res, next) {
-    Show.findById(req.body.showId, function(err, show) {
-      if (err) return next(err);
-      var index = show.subscribers.indexOf(req.user._id);
-      show.subscribers.splice(index, 1);
-      show.save(function(err) {
-        if (err) return next(err);
         res.send(200);
       });
     });
--- server/server.js
+++ server/server.js
@@ -2,10 +2,12 @@ var path = require('path');
 var express = require('express');
 var bodyParser = require('body-parser');
 var logger = require('morgan');
+var cookieParser = require('cookie-parser');
 
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
+app.use(cookieParser());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
