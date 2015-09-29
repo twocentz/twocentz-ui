3b1d68fcec77fe167b7fbd457a344b2439c88956
Ilyas Khawaja <kmilyas@gmail.com>
2015-09-28 22:08:58 -0400

    now able to submit entries on the movies page. Yay!

--- public/scripts/controllers/movies.js
+++ public/scripts/controllers/movies.js
@@ -7,68 +7,42 @@ angular.module('MyApp')
      * Get movie detail.
      */
     Topic.getMovieBySlug($stateParams.slug)
-    .then(function(data) {
-      if(data.error){
-        $scope.error = true;
-      }else{
-        $scope.topic = data;
-        document.title = $scope.topic.title + " - TwoCentz";
+      .then(function(data) {
+        if(data.error){
+          $scope.error = true;
 
-        // Entries.getMovieEntryById($scope.topic.id)
-        // .then(function(data){
-        //   if(!data.error){
-        //     $scope.entries = data.content;
-        //     $scope.words = [];
-        //     _.each($scope.entries, function(entry){
-        //       $scope.words.push({text: entry.text, weight: entry.votes});
-        //     });
-        //   } else {
-            
-        //   }
-        // });
+        }else{
+          
+          $scope.topic = data;
+          $scope.words = populateWordCloud($scope.topic.topEntries);
+          $scope.userVoted = [];
 
-        // make a call to user to get user entries for this topic
+          document.title = $scope.topic.title + " - TwoCentz";
 
+          // Entries.getMovieEntryById($scope.topic.id)
+          // .then(function(data){
+          //   if(!data.error){
+          //     $scope.entries = data.content;
+          //     $scope.words = [];
+          //     _.each($scope.entries, function(entry){
+          //       $scope.words.push({text: entry.text, weight: entry.votes});
+          //     });
+          //   } else {
+              
+          //   }
+          // });
 
-        // fake data
-        $scope.entries = [
-          {
-              "text": "superb movie",
-              "votes": 25
-          },
-          {
-              "text": "great acting",
-              "votes": 12
-          },
-          {
-              "text": "nonsense storyline",
-              "votes": 9
-          },
-          {
-              "text": "lazy director",
-              "votes": 6
-          },
-          {
-              "text": "just wow",
-              "votes": 3
-          },
-          {
-              "text": "stupid action",
-              "votes": 2
-          }
-        ];
-    
-        $scope.words = populateWordCloud($scope.entries);
-        $scope.userVoted = [];
-        // end fake data
-      }
-    });
+          // TODO: make a call to user entries to get user voted entries list
+          // $scope.userVoted = [get from DB];
+        }
+      });
+
     $scope.tc = {
       text: "",
       submited: false
     }
 
-    // no of entries to show
+    // page properties. 
     $scope.list = {
       text: "show all",
       text_all: "show all",
@@ -78,19 +52,6 @@ angular.module('MyApp')
       max: 100,
       isMax: false
     }
-
-    //$scope.userVoted = ["nonsense storyline", "just wow"];
-
-    // $scope.topic = {
-    //       "id"  : "ABC123", 
-    //       "title" : "The Running Man",
-    //       "description": "A wrongly convicted man must try to survive a public execution gauntlet staged as a game show.",
-    //       "tags": ["movie", "sci-fi", "action", "arnold"],
-    //       "images": ["http://ia.media-imdb.com/images/M/MV5BNzM4OTcyMjEyNl5BMl5BanBnXkFtZTcwMzEwNDI4OA@@._V1_SX214_AL_.jpg"]
-    //   };
-    // document.title =  $scope.topic.title + " - TwoCentz";
-
-    
 
     $scope.keySubmit = function(keyEvent){
       if (keyEvent.which === 13){
@@ -117,24 +78,24 @@ angular.module('MyApp')
 
       if($scope.tc.text !== null && $scope.tc.text.length>0){
         Entries.postEntriesByTopicId($scope.tc.text, $scope.topic.id)
-        .then(function(data){
-          if(data.created === 'true'){
-            
-            addEntryLocally($scope.tc.text, $scope.entries, $scope.userVoted);
-            $scope.entries = sortEntries($scope.entries);
-            $scope.words = populateWordCloud($scope.entries);
-            $('.label-success').show().addClass('animated pulse').delay(2000).fadeOut(1000);
+          .then(function(data){
+            if(data.created === 'true'){
+              
+              addEntryLocally($scope.tc.text, $scope.topic.topEntries, $scope.userVoted);
+              $scope.topic.topEntries = sortEntries($scope.topic.topEntries);
+              $scope.words = populateWordCloud($scope.topic.topEntries);
+              $('.label-success').show().addClass('animated pulse').delay(2000).fadeOut(1000);
 
-          }else{
+            }else{
 
-            //incase of error
-            // console.log(data.status + " " + data.error);
-            $('.label-danger').show().addClass('animated shake').delay(2000).fadeOut(1000);
-          }
-          // reseting submit widget value
-          $scope.tc.text = "";
-          $scope.tc.submited = false;
-        });
+              //incase of error
+              // console.log(data.status + " " + data.error);
+              $('.label-danger').show().addClass('animated shake').delay(2000).fadeOut(1000);
+            }
+            // reseting submit widget value
+            $scope.tc.text = "";
+            $scope.tc.submited = false;
+          });
       }
     };
 
