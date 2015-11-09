62fff9ac7bcd2f39a45c5764d4b6cce0669aa3bc
Ilyas Khawaja <kmilyas@gmail.com>
2015-11-08 21:39:03 -0500

    logging error

--- public/scripts/controllers/movies.js
+++ public/scripts/controllers/movies.js
@@ -32,7 +32,7 @@
               User.getUserEntriesByTopicId($scope.topic.id)
                 .then(function (data){
                   if(data.error){
-                    $scope.error = true;
+                    console.log($scope.error);
                   } else {
                     $scope.userVoted = _.pluck(data.content, 'text');
                   }
