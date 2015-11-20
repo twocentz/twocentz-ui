91810ff357737562840d6bd305598b5d7e734153
Ilyas Khawaja <kmilyas@gmail.com>
2015-11-19 22:58:02 -0500

    adding https redirect for heroku

--- server/server.js
+++ server/server.js
@@ -4,7 +4,11 @@ var bodyParser = require('body-parser');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 
+var env = process.env.NODE_ENV || 'development';
 var app = express();
+
+
+
 
 app.set('port', process.env.PORT || 3000);
 app.use(cookieParser());
@@ -16,6 +20,15 @@ app.use(express.static(path.join( path.normalize(__dirname + '/..'), 'dist')));
 // console.log("***** App Path *********" + path.join( path.normalize(__dirname + '/..'), 'dist'));
 // console.log("***** ENV *********" + process.env.STORMPATH_API_KEY_ID);
 
+if (env === 'production') {
+  app.get('*',function(req,res,next){
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      console.log("redirect: to https");
+      return res.redirect(['https://', req.get('Host'), req.url].join(''));
+    }
+    return next();
+  })
+}
 
 require('./routes')(app);
 
