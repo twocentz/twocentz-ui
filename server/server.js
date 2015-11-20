18d017a962f7cd72cf061926a4a3085c7a1e0be1
Ilyas Khawaja <kmilyas@gmail.com>
2015-11-19 23:50:26 -0500

    added nodejs server side logging through logentries

--- server/server.js
+++ server/server.js
@@ -3,11 +3,15 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
+var Logger = require('le_node');
+var log = new Logger({
+  token:'28364857-ccad-34ad-b844-44bb3a088cf1',
+  console: true,
+  timestamp: true
+});
 
 var env = process.env.NODE_ENV || 'development';
 var app = express();
-
-
 
 
 app.set('port', process.env.PORT || 3000);
@@ -23,7 +27,7 @@ app.use(express.static(path.join( path.normalize(__dirname + '/..'), 'dist')));
 if (env === 'production') {
   app.get('*',function(req,res,next){
     if (req.headers['x-forwarded-proto'] !== 'https') {
-      console.log("redirect: to https");
+      log.info('server-'+ env +': redirect to https');
       return res.redirect(['https://', req.get('Host'), req.url].join(''));
     }
     return next();
@@ -33,5 +37,6 @@ if (env === 'production') {
 require('./routes')(app);
 
 app.listen(app.get('port'), function() {
-  console.log('Express server listening on port ' + app.get('port'));
+  log.info('server-'+ env +': Express server listening on port ' + app.get('port'));
+  //console.log('Express server listening on port ' + app.get('port'));
 });
