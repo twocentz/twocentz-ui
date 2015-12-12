ec8b66f38e7939366919d3e332dad04c096c90ee
Ilyas Khawaja <kmilyas@gmail.com>
2015-12-12 18:30:47 -0500

    making https redirect work

--- server/server.js
+++ server/server.js
@@ -12,6 +12,15 @@ var log = new Logger({
 var env = process.env.NODE_ENV || 'development';
 var app = express();
 
+if (env === 'production') {
+  app.get('*',function(req,res,next){
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      log.info('server-'+ env +': redirect to https');
+      return res.redirect(['https://', req.get('Host'), req.url].join(''));
+    }
+    return next();
+  })
+}
 
 app.set('port', process.env.PORT || 3000);
 app.use(cookieParser());
@@ -23,15 +32,7 @@ app.use(express.static(path.join( path.normalize(__dirname + '/..'), 'dist')));
 // console.log("***** App Path *********" + path.join( path.normalize(__dirname + '/..'), 'dist'));
 // console.log("***** ENV *********" + process.env.STORMPATH_API_KEY_ID);
 
-if (env === 'production') {
-  app.get('*',function(req,res,next){
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      log.info('server-'+ env +': redirect to https');
-      return res.redirect(['https://', req.get('Host'), req.url].join(''));
-    }
-    return next();
-  })
-}
+
 
 require('./routes')(app);
 
