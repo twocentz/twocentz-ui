28054c550ef2e7e5bcb627d73496b61af8242c22
Ilyas Khawaja <kmilyas@gmail.com>
2016-02-02 22:09:13 -0500

    simplified topic creation to one field only

--- public/scripts/controllers/add.js
+++ public/scripts/controllers/add.js
@@ -74,29 +74,6 @@
 
         return deferred.promise;
 
-        // if (!$scope.files) return;
-        // angular.forEach(files, function(file){
-        //   if (file && !file.$error) {
-        //     file.upload = Upload.upload({
-        //       url: "https://api.cloudinary.com/v1_1/" + $.cloudinary.config().cloud_name + "/upload",
-        //       fields: {
-        //         upload_preset: $.cloudinary.config().upload_preset,
-        //         context: 'alt=' + $scope.title
-        //       },
-        //       file: file
-        //     }).progress(function (e) {
-        //       file.progress = Math.round((e.loaded * 100.0) / e.total);
-        //       file.status = "Uploading... " + file.progress + "%";
-        //     }).success(function (data, status, headers, config) {
-        //       $rootScope.photos = $rootScope.photos || [];
-        //       data.context = {custom: {photo: $scope.title}};
-        //       file.result = data;
-        //       $rootScope.photos.push(data);
-        //     }).error(function (data, status, headers, config) {
-        //       file.result = data;
-        //     });
-        //   }
-        // });
       };
 
       /* Modify the look and fill of the dropzone when files are being dragged over it */
@@ -119,105 +96,14 @@
 
       function init() {
         vm.model = {};
-
-        // vm.model = {
-        //   //textField: [
-        //   //  {
-        //   //    fieldName:'',
-        //   //    fieldValue:''
-        //   //  }
-        //   //],
-        //   textArea: [
-        //     {
-        //       fieldName:'',
-        //       fieldValue:''
-        //     }
-        //   ]
-        // };
-
-
         vm.fields = [
           {
             key: 'title',
-            type: 'input',
-            templateOptions: {
-              label: 'Title',
-              placeholder: 'your topics title',
-              required: true
-            }
-          },
-          {
-            key: 'description',
             type: 'textarea',
             templateOptions: {
-              label: 'Description',
-              placeholder: 'Some description of the topic'
-            }
-          },
-          //{
-          //  type: 'propertySection',
-          //  key: 'textField',
-          //  templateOptions: {
-          //    btnText:'new field',
-          //    fields: [
-          //      {
-          //        className: 'form-inline',
-          //        fieldGroup: [
-          //          {
-          //            type: 'input',
-          //            key: 'fieldName',
-          //            templateOptions: {
-          //              label: 'Label',
-          //              placeholder: 'your field label',
-          //              required: true
-          //            }
-          //          },
-          //          {
-          //            type: 'input',
-          //            key: 'fieldValue',
-          //            templateOptions: {
-          //              label: 'Value',
-          //              placeholder: 'your field value',
-          //              required: true
-          //            }
-          //          }
-          //        ]
-          //      }
-          //    ]
-          //  }
-          //},
-          {
-            type: 'propertySection',
-            key: 'textArea',
-            templateOptions: {
-              btnText:'new text area',
-              fields: [
-                {
-                  className: 'form-inline',
-                  fieldGroup: [
-                    {
-                      type: 'input',
-                      key: 'fieldName',
-                      templateOptions: {
-                        label: 'Label',
-                        placeholder: 'your field label',
-                        required: true
-                      }
-                    },
-                    {
-                      type: 'textarea',
-                      key: 'fieldValue',
-                      templateOptions: {
-                        label: 'Value',
-                        placeholder: 'your field value',
-                        required: true,
-                        rows: 4,
-                        cols: 20
-                      }
-                    }
-                  ]
-                }
-              ]
+              label: 'Post',
+              placeholder: 'What do you want to share?',
+              required: true
             }
           }
         ];
@@ -228,7 +114,7 @@
     vm.onSubmit = onSubmit;
     vm.options = {};
     init();
-    vm.originalFields = angular.copy(vm.fields);
+    //vm.originalFields = angular.copy(vm.fields);
 	}
   angular
     .module('TwoCentzWeb')
